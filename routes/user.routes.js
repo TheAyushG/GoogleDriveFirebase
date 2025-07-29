39513b27e8931a5335ed4a816145c55295752f3d
TheAyushG <jiaayush256@gmail.com>
2025-07-29 23:40:34 +0530

    update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router();
-
 const { body, validationResult } = require('express-validator');
+const userModel = require('../models/user.model')
+const bcrypt = require('bcrypt');
 
 /* /user/test */
 router.get('/register', (req,res) => {
@@ -12,15 +13,32 @@ router.post('/register',
     body('email').trim().isEmail().isLength({ min: 13}),
     body('password').trim().isLength({ min: 5 }),
     body('username').trim().isLength({ min: 3}),
-    (req,res) => {
+    async (req,res) => {
 
     const errors = validationResult(req);
     
     if(!errors.isEmpty()){
-        return res.send("Inavalid data")
+        return res.status(400).json({
+            errors: errors.array(),
+            message: 'Invalid data'
+        })
     }
 
-    res.send(errors)
+    const {email, username, password} = req.body;
+
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    const newUser = await userModel.create({
+        email,
+        username,
+        password: hashPassword
+    })
+    res.json(newUser)
 })
+
+
+router.get('/login',(req,res) => {
+    res.render('login')
+} )
 
 module.exports = router; //we export this route from here and we import it in app.js
